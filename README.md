578c37aace51b4dc0fc9ef6f83109ccbfbff02f1
carole tierney <caroleatierney@gmail.com>
2023-08-30 18:44:48 -0400

    many issues with update and delete

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Week-12-Project
 
+start server
+
+
 http://www.nenature.com/ButterfliesByFamily.htm
 
 add an array of sub family names with names & images
+
+Instead of a table, look into organizing the students differently with bootstrap:
+Card, Accordion, Dropdowns, Popover, Tooltips
+
+ * Tables: https://getbootstrap.com/docs/5.3/content/tables/#overview
+ * Forms: https://getbootstrap.com/docs/5.3/forms/overview/#overview
+
+Form handling???
+-Dups
+-required fields
---
+++ app.js
@@ -0,0 +1,120 @@
+// set up local endpoint
+
+const URL_ENDPOINT = 'http://localhost:3000/newEnglandButterflies'
+
+//  get and display file (to see if it is linked)
+// $.get(URL_ENDPOINT).then(data => console.log(data))
+
+// Build Table
+$.get(URL_ENDPOINT).then(data => {
+    data.map(newEnglandButterflies => {
+        $('tbody').append(
+            $(`
+            <tr>
+                <td>${newEnglandButterflies.id}</td>
+                <td>${newEnglandButterflies.name}</td>
+                <td>${newEnglandButterflies.familyName}</td>
+                <td>${newEnglandButterflies.commonExample}</td>
+                <td><button onclick="updateButterflyForm(${newEnglandButterflies.id})">Update</button></td>
+                <td><button onclick="deleteButterfly(${newEnglandButterflies.id})">Delete</button></td>
+            </tr>
+            `)
+        )
+    })
+})
+
+function addNewButterflyForm() {
+    $('#add').remove();
+
+    $('#formCont').append(
+        $(`
+        <form>
+        <h1>Add a new butterfly</h1>
+            <div>
+                <label for="name">Butterly Name</label>
+                <input id ="name" placeholder="Butterly Name"/>
+            </div>
+
+            <div>
+                <label for="familyName">Family Name</label>
+                <input id ="familyName" placeholder="Family Name"/>
+            </div>
+
+            <div>
+                <label for="commonExample">Common Example</label>
+                <input id ="commonExample" placeholder="Common Example"/>
+            </div>
+
+            <button onclick="postButterfly()">Submit</button>
+        </form>
+        `)
+    )
+}
+
+// add to db
+function postButterfly() {
+    $.post(URL_ENDPOINT, {
+        name: $('#name').val(),
+        familyName: $('#familyName').val(),
+        commonExample: $('#commonExample').val()
+    })
+}
+
+// update form
+function updateButterflyForm(id) {
+    $('#add').remove();
+
+    $('#formCont').append(
+        $(`
+        <form>
+        <h1>Update a butterfly</h1>
+            <div>
+                <label for="updateId">Butterly Id</label>
+                <input id ="updateId" placeholder="Butterly Id" value="${id}"/>
+            </div>
+
+            <div>
+                <label for="updateName">Butterly Name</label>
+                // <input id ="updateName" placeholder="Butterly Name" value="${id}"/>
+                <input id ="updateName" placeholder="Butterly Name"/>
+            </div>
+            <div>
+                <label for="updateFamilyName">Family Name</label>
+                <input id ="updateFamilyName" placeholder="Family Name" value="${id}"/>
+            </div>
+
+            <div>
+                <label for="updateCommonExample">Common Example</label>
+                <input id ="updateCommonExample" placeholder="Common Example" value="${id}"/>
+            </div>
+
+            <button onclick="putButterfly(id)">Submit</button>
+        </form>
+        `)
+    )
+}
+
+// update db
+function putButterfly(id) {
+    $.ajax(`{$URL_ENDPOINT}/${id}`,  {
+        method: 'PUT',
+        data: {
+            name: $('#name').val(),
+            familyName: $('#familyName').val(),
+            commonExample: $('#commonExample').val()
+        }
+    })
+}
+
+// delete db
+function deleteButterfly(id) {
+    $('#add').remove();
+
+    // works
+    // let del = $.ajax(`{URL_ENDPOINT}/${id}`)
+    // console.log('del', del)
+
+    $.ajax(`{URL_ENDPOINT}/${id}`, {
+        method: 'DELETE',
+    });
+}
